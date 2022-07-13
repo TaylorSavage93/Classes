e54f52f1202c05614146c13e84f9bf65dd599e4a
Taylor Savage <taylorsavage93@gmail.com>
2022-07-13 15:02:46 -0400

    finished exercise 1

---
+++ Classes_Ex1/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Demo.Net6;
+
+internal class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+
+
+
+
+}
---
+++ Classes_Ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Classes_Demo.Net6;
+
+Car car1 = new Car();
+car1.Make = "Ford";
+car1.Model = "Bronco";
+car1.Year = 1973;
+
+Console.WriteLine($"My favorite car is the {car1.Make} {car1.Model},specifically the {car1.Year} version.");
